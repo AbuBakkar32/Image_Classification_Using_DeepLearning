591ba2e0c9126814bd6d774617d249c7dbbc819e
abubakkar32 <abu35-1994@diu.edu.bd>
2021-01-17 11:48:00 +0600

    no message

--- ICUDL/Images/views.py
+++ ICUDL/Images/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ImageUploadForm
 
 
 # Create your views here.
@@ -6,6 +7,14 @@ def home(request):
     return render(request, 'home.html')
 
 
-def imageprocess(request):
+def handle_image_file(f):
+    with open('image.jpg', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
-    return True
+
+def imageprocess(request):
+    form = ImageUploadForm(request.POST, request.FILES)
+    if form.is_valid():
+        handle_image_file(request.FILES['image'])
+    return render(request, 'result.html')
